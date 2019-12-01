e9592bf2ebcc8cba79c35b47529af3c4d2fe2bbe
Wessam Ibrahim <wessami@lana.games>
2019-12-01 23:57:45 +0100

    Using system to spawn projectiles - WIP

---
+++ Assets/Scripts/DOTS/Components/ProjectileSpawner.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public struct ProjectileSpawnerComponent : IComponentData
+{
+	public float Speed;
+	public float Lifetime;
+	public float3 Force;
+	public float MaxDamage;
+	public float Radius;
+	public int Health;
+	public Entity Prefab;
+}
--- Assets/Scripts/DOTS/Spawners/ProjectileSpawner_FromMonoBehaviour.cs
+++ Assets/Scripts/DOTS/Spawners/ProjectileSpawner_FromMonoBehaviour.cs
@@ -9,6 +9,8 @@ public class ProjectileSpawner_FromMonoBehaviour : MonoBehaviour
 	public AudioSource SFX;
 	public ParticleSystem VFX;
 
+	public float Speed;
+	public float Lifetime;
 	public float MaxDamage;
 	public float Radius;
 	public int Health;
@@ -20,17 +22,16 @@ public class ProjectileSpawner_FromMonoBehaviour : MonoBehaviour
 		var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(Prefab, World.Active);
 		var entityManager = World.Active.EntityManager;
 
-		// Efficiently instantiate a bunch of entities from the already converted entity prefab
 		var instance = entityManager.Instantiate(prefab);
 
-		// Place the instantiated entity in a grid with some noise
-		var position = transform.TransformPoint(new float3(1.3F, noise.cnoise(new float2(1, 1) * 0.21F) * 2, 1.3F));
-		entityManager.SetComponentData(instance, new Translation { Value = position });
-		entityManager.AddComponentData(instance, new ProjectileSpeedComponent { MetersPerSecond = 1.0f });
-		entityManager.AddComponentData(instance, new ProjectileLifetimeComponent { Seconds = 2.0f });
-		entityManager.AddComponentData(instance, new ProjectileDamageComponent { Force = Force, MaxDamage = MaxDamage, Radius = Radius, Health = Health });
-
-		entityManager.AddSharedComponentData(instance, new ProjectileExplosionAudioComponent { m_ExplosionAudio = SFX });
-		entityManager.AddSharedComponentData(instance, new ProjectileExplosionParticleComponent { m_ExplosionParticles = VFX });
+		entityManager.AddComponentData(instance, new ProjectileSpawnerComponent { 
+			Speed = Speed,
+			Lifetime = Lifetime, 
+			MaxDamage = MaxDamage, 
+			Radius = Radius, 
+			Force = Force, 
+			Health = Health,
+			Prefab = prefab
+		});
 	}
 }
